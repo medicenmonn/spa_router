3686d8d87a046ddb123e1a249277f800db4fc3a9
Mon Salgado <61484694+medicenmonn@users.noreply.github.com>
2020-10-12 21:54:21 -0500

    creacion de router para SPA

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [     //declarar un array para poner las rutas qude hacen ref directa
+    {
+        path: '/',
+        template: '<h1>Hola</h1>'
+    },
+    {
+        path: '/contacto',
+        template: '<h1>Contacto</h1>'
+    },
+    {
+        path: '/about',
+        template: '<h1>About me</h1>'
+    },
+];
